c4fc246bd4f9f9802e4dc42c85336a4a1c277519
tshoniunathi <143700375+tshoniunathi@users.noreply.github.com>
2024-05-13 15:50:55 +0200

    updated javascript

--- index.js
+++ index.js
@@ -9,15 +9,13 @@ function refreshWeather(response) {
 
 function searchCity(city) {
   let apiKey = "c43872bf04t88doa0e696badf333b227";
-  let apiUrl = `"https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric"`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(refreshWeather);
 }
 
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
   searchCity(searchInput.value);
 }
 
