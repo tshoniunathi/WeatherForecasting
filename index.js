75ff59ba69a40a5be8e528ad956d92bf68aaf149
tshoniunathi <143700375+tshoniunathi@users.noreply.github.com>
2024-05-19 18:00:08 +0200

    update script

--- index.js
+++ index.js
@@ -57,10 +57,11 @@ function searchCity(city) {
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
+
   searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
-searchCity(searchInput.value);
+searchCity("Paris");
